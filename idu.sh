3cad3741525b476cc89f1a2e2da92a2218e90f96
Iressa <121632031+Iressa@users.noreply.github.com>
2024-03-17 20:10:35 +0200

    feat: Add image handling

--- idu.sh
+++ idu.sh
@@ -2,35 +2,56 @@
 
 # Create directory if doesn't exist yet.
 function create_dir() {
-    if [[ ! -e project_$1 ]]
+    local name=$(echo $1 | sed -e "s/.md//g")
+    local project_name=$(echo "project_"$name)
+
+    # Create main project directory.
+    if [[ ! -e "$project_name" ]]
     then
-        mkdir `echo project_$1`
+        mkdir $project_name
     fi
 
-    if [[ ! -e project_$1/lang ]]
+    # Create language subdirectory.
+    if [[ ! -e `echo $project_name"/lang"` ]]
     then
-        mkdir `echo project_$1/lang`
+        mkdir `echo $project_name/lang`
     fi
 
-    if [[ ! -e project_$1/resources ]]
+    # Create resource subdirectory.
+    if [[ ! -e `echo $project_name"/resources"` ]]
     then
-        mkdir `echo project_$1/resources`
+        mkdir `echo $project_name/resources`
+    fi
+
+    echo "$project_name"
+}
+
+function images() {
+    if [[ -z "$1" ]]
+    then
+        echo "Failed to extract images: No input given."
+        exit
+    fi
+
+    local filename=$1
+    local arr=$(sed "s/\!\[/\n\!\[/g" `echo "docs/"$filename` | grep static/img | sed "s/.*(//" | sed "s/).*//")
+
+    for i in ${arr[@]}; do
+        cp $i $2
+    done
+}
+
+# Get main file.
+function get_main_file() {
+    if [[ -e `echo "docs"/$name` ]]
+    then
+        cp `echo "docs"/$name` `echo $project_name/$name`
+    else
+        vim `echo $project_name/$name`
     fi
 }
 
-function open() {
-    # Get name of file to open.
-    name=`ls -a docs|grep -E ".*.md"|fzf --print-query|tail -1`
-
-   create_dir $name
-
-    if [[ -e `echo "docs"/$name` ]]
-    then
-        cp `echo "docs"/$name` `echo "project_"$name/$name`
-    else
-        vim `echo "docs"/$name`
-    fi
-
+function get_lang_files() {
     locales=`grep locales docusaurus.config.js|cut -c 14-`
     localesstring=$(echo $locales | sed -e 's/\[//g' -e 's/\]//g' -e 's/\,//g')
     arr=( $localesstring )
@@ -43,9 +64,36 @@ function open() {
         # with the locale code prefixed e.g., fi_hello.md.
         if [[ -e `echo $locale_wd/$name` ]]
         then
-            cp `echo $locale_wd/$name` `echo project_$name/lang/$i"_"$name`
+            cp `echo $locale_wd/$name` `echo $project_name"/lang/"$i"_"$name`
         fi
     done
+}
+
+function open() {
+
+    # Prompt for file name if not given.
+    if [[ -z "$1" ]]
+    then
+        name=`ls -a docs|grep -E ".*.md"|fzf --print-query|tail -1`
+    elif [[ -e "$1" ]]
+    then
+        name=$($1)
+    else
+        echo $("Given filename $1 is not valid.")
+        exit
+    fi
+
+    # Initialize project directory if does not exist.
+    project_name=$(create_dir $name)
+
+    # Copy the main file to the project directory.
+    get_main_file
+
+    # Copy the localized files to the project directory.
+    get_lang_files
+
+    # Copy the image files to the project directory.
+    images $name `echo $project_name`
 }
 
 function commit() {
@@ -92,14 +140,13 @@ usage: idu [command] [-h] [-rm]
 
 if [[ $1 == "open" ]]
 then
-    open
+    open $2
 elif [[ $1 == "commit" ]]
 then
     commit $2
 elif [[ $1 == "-h" ]]||[[ $1 == "--help" ]]
 then
     display_help
-
 else
     echo "Use -h or --help for help."
 fi
